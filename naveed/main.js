16f25b989f36554bd01c479bdb2022bc5d68c932
Naveed Ahmed <161067042+Navtarib@users.noreply.github.com>
2024-10-13 16:12:33 +0500

    Add files via upload

---
+++ naveed/main.js
@@ -0,0 +1,38 @@
+// function sum(a,b){document.write(a+b);}
+//  function studentsName(student1,student2,student3){
+//     document.write(student1,student2,student3)
+
+//  }
+
+var value1 ='' 
+var value2 =''
+function val1(){
+    var v1= Number(prompt('Enter 1st value'));
+    value1 =v1;
+}
+function val2(){
+    var v2= Number(prompt('Enter 2nd value'));
+    value2 =v2;
+}
+function sum(){
+alert(`result ${value1+value2}`
+
+)}
+
+function sub(){
+    alert(`result ${value1-value2}`
+    
+    )}
+
+    
+    function multiply(){
+        alert(`result ${value1*value2}`
+        
+        )}
+
+        
+        function divide(){
+            alert(`result ${value1/value2}`
+            
+            )}
+            
